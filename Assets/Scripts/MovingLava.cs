4e5c41e19404b5cd7625f669c34632d75465ea43
Katanchick23 <andrey.sherstobitov.2017@mail.ru>
2022-06-28 23:19:36 +0300

    <Last update>

--- Assets/Scripts/MovingLava.cs
+++ Assets/Scripts/MovingLava.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class MovingLava : MonoBehaviour
 {
     void Update(){
-        transform.position = new Vector3(transform.position.x, Mathf.Lerp(transform.position.y, transform.position.y + 5, 0.001f), transform.position.z);
+        transform.position = new Vector3(transform.position.x, Mathf.Lerp(transform.position.y, transform.position.y + 5, 0.0005f), transform.position.z);
     }
 
 }
